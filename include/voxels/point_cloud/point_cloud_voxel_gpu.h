462a9f41f3e6c8e22e3e82e10794b1761e0b67b0
Siddharth Diwan <70694457+sidwan02@users.noreply.github.com>
2023-12-07 18:30:54 -0500

    passing all tests except the expansion 2 gpu

--- include/voxels/point_cloud/point_cloud_voxel_gpu.h
+++ include/voxels/point_cloud/point_cloud_voxel_gpu.h
@@ -3,15 +3,16 @@
 #include <cuda.h>
 #include <vector>
 
-#include "constants.h"
 #include "point.h"
 #include "voxels/voxel.h"
 #include "voxels/voxel_manager.h"
 
-namespace grrt {
+namespace grrt
+{
 
-    class PointCloudVoxelGPU : public Voxel {
-       public:
+    class PointCloudVoxelGPU : public Voxel
+    {
+    public:
         typedef std::shared_ptr<PointCloudVoxelGPU> SharedPtr;
 
         PointCloudVoxelGPU(const size_t num_points);
@@ -21,30 +22,32 @@ namespace grrt {
         /// @brief Gets the radius of each point in the point cloud, in meters.
         inline float getRadius() const { return m_radius; }
 
-        void addPoint(const Point& point);
+        void addPoint(const Point &point);
 
-        bool contains(const Point& point) const override;
+        bool contains(const Point &point) const override;
 
         const size_t num_points = 0;
-        float* points = nullptr;
+        float *points = nullptr;
 
         Point start_point;
         Point end_point;
 
-       private:
+    private:
         const float m_radius = PCL_VOXEL_RADIUS;
 
         size_t current_num_points = 0;
     };
 
-    class PointCloudVoxelGPUManager : public VoxelManager {
-       public:
+    class PointCloudVoxelGPUManager : public VoxelManager
+    {
+    public:
         typedef std::shared_ptr<PointCloudVoxelGPUManager> SharedPtr;
 
         // defined in cu file.
-        bool intersect(const Voxel::SharedPtr& voxel_1, const Voxel::SharedPtr& voxel_2) override;
+        bool intersect(const Voxel::SharedPtr &voxel_1, const Voxel::SharedPtr &voxel_2) override;
 
-        Voxel::SharedPtr optimize(Voxel::SharedPtr& voxel) override {
+        Voxel::SharedPtr optimize(Voxel::SharedPtr &voxel) override
+        {
             // TODO
             return nullptr;
         }
@@ -53,4 +56,4 @@ namespace grrt {
     // for the gpu variants, it will call the cuda kernel.
 
     // All the cuda kernal stuff needs to be in not hpp files.
-}  // namespace grrt
+} // namespace grrt
